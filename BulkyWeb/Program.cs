e869feb28dcc9609bc17d8a212a6c901995f531c
Ammar <smakali1999@gmail.com>
2024-05-23 12:31:10 +0500

    Added Identity

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,11 +1,14 @@
 using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
